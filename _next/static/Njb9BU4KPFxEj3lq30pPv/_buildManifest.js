1b70746f74ef09580daf0bad2ed1806e11ec95a0
Error Dev <3rr0r.d3v@gmail.com>
2025-06-04 22:38:45 +1000

    Updates

---
+++ _next/static/Njb9BU4KPFxEj3lq30pPv/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/954-3962f4feafd65ffb.js","static/chunks/pages/index-7c9904fb17d66998.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
